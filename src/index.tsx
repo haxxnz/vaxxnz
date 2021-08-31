88b132ea822b13dd81fd5e8c34290181bf3ec3f7
Walter Lim <waltissomewhere@gmail.com>
2021-08-31 23:30:26 +1200

    push logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider } from 'baseui';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const engine = new Styletron();
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
