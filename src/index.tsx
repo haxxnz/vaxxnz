3ae1acb2f8cbb2561e1897261dcba09234b2de89
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-01 00:06:29 +1200

    add provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,36 @@
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { Provider as StyletronProvider } from 'styletron-react';
-import { LightTheme, BaseProvider } from 'baseui';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { DateLocationsPairsContext } from './contexts';
-
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DateLocationsPairsContext } from "./contexts";
+import { DateLocationsPair } from "./types";
 
 const engine = new Styletron();
 
+function Index() {
+  const [dateLocationsPairs, setDateLocationsPairs] = React.useState<
+    DateLocationsPair[]
+  >([]);
+  return (
+    <React.StrictMode>
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={LightTheme}>
+          <DateLocationsPairsContext.Provider
+            value={{ dateLocationsPairs, setDateLocationsPairs }}
+          >
+            <App />
+          </DateLocationsPairsContext.Provider>
+        </BaseProvider>
+      </StyletronProvider>
+    </React.StrictMode>
+  );
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-
-    <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}>
-        <DateLocationsPairsContext.Provider>
-          <App />
-        </DateLocationsPairsContext.Provider>
-
-      </BaseProvider>
-    </StyletronProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
