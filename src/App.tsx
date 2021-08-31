faf982d5ba7c111f6d85ee03c60bbe364e350249
Walter Lim <waltissomewhere@gmail.com>
2021-08-31 20:30:46 +1200

    initia calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,459 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        <h1>Vaccine Timetable</h1>
+        <p>Check vaccine timetable availability before making a booking. This is an unofficial website and pulls data from managemyvaccine.nz </p>
+      </section>
+      <HeaderMain>
+        <h1>Available Vaccine Slots</h1>
+
+      </HeaderMain>
+      <CalendarContainer>
+        <CalendarSectionContainer>
+          <h2>September 2021</h2>
+          <MonthContainer>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+
+
+
+          </MonthContainer>
+        </CalendarSectionContainer>
+        <CalendarSectionContainer>
+          <h2>October 2021</h2>
+          <MonthContainer>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+
+
+
+          </MonthContainer>
+        </CalendarSectionContainer>
+        <CalendarSectionContainer>
+          <h2>September 2021</h2>
+          <MonthContainer>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+
+
+
+          </MonthContainer>
+        </CalendarSectionContainer>
+        <CalendarSectionContainer>
+          <h2>September 2021</h2>
+          <MonthContainer>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+            <div>
+              <h3>1 Sept</h3>
+              <p>3 slots available</p>
+            </div>
+
+
+
+          </MonthContainer>
+        </CalendarSectionContainer>
+
+
+      </CalendarContainer>
+
     </div>
   );
 }
 
 export default App;
+
+
+const HeaderMain = styled.header`
+position: sticky;
+box-sizing: border-box;
+top: -2px;
+padding: 1.5rem;
+background-color: white;
+
+  z-index: 3;
+
+border-top: 1px solid lightgray;
+border-bottom: 1px solid lightgray;
+h1 {
+  text-align: left;
+  margin: 0;
+}
+`
+
+const CalendarContainer = styled.section`
+
+  margin:  0;
+
+  `
+
+
+
+const CalendarSectionContainer = styled.section`
+h2 {
+  display: block;
+  padding: 1.5rem ;
+  font-size: 1.5rem;
+border-bottom: 1px solid lightgray;
+position: sticky;
+top:86px;
+
+  background-color: #fff;
+  z-index: 1;
+}
+`
+
+const MonthContainer = styled.section`
+display: grid;
+box-sizing: border-box;
+grid-template-columns: repeat(7, 1fr);
+background-color: lightgray;
+border-bottom: 1px solid lightgray;
+gap: 1px;
+div {
+  min-height:120px;
+  background-color: white;
+  padding: 1.5rem;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h3 {
+    font-size: 1.2rem;
+    font-weight: 400;
+  }
+  p {
+    font-size: 1.2rem;
+    font-weight: 600;
+  }
+}
+  `
