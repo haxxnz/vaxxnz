3d612db0d1fef5e53b58a84e5cfbe808df76b311
minh <Lminhphu0304@gmail.com>
2021-09-06 19:07:07 +1200

    VsCode screws up spacing

--- src/WalkSection.tsx
+++ src/WalkSection.tsx
@@ -6,157 +6,157 @@ import WalkModal from "./WalkModal";
 import { getDistanceKm } from "./distanceUtils";
 
 export interface Props {
-    lat: number;
-    lng: number;
-    radiusKm: number;
+  lat: number;
+  lng: number;
+  radiusKm: number;
 }
 
 export function WalkInSection({ lat, lng, radiusKm }: Props) {
-    const [selectedLocationIndex, setSelectedLocation] = useState<number>();
-    const [walkInLocations, setWalkinLocation] = useState<WalkinLocation[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    useEffect(() => {
-        setLoading(true);
-        getWalkinData()
-            .then((walkIn) => {
-                const matchedFilter = walkIn.filter(
-                    ({
-                        lat: locationLat,
-                        lng: locationLng,
-                        isOpenToday,
-                        instructionLis,
-                    }) => {
-                        const distanceInKm =
-                            locationLat &&
-                            locationLng &&
-                            getDistanceKm(lat, lng, locationLat, locationLng);
+  const [selectedLocationIndex, setSelectedLocation] = useState<number>();
+  const [walkInLocations, setWalkinLocation] = useState<WalkinLocation[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(true);
+    getWalkinData()
+      .then((walkIn) => {
+        const matchedFilter = walkIn.filter(
+          ({
+            lat: locationLat,
+            lng: locationLng,
+            isOpenToday,
+            instructionLis,
+          }) => {
+            const distanceInKm =
+              locationLat &&
+              locationLng &&
+              getDistanceKm(lat, lng, locationLat, locationLng);
 
-                        const walkInOrDriveThru =
-                            instructionLis.includes("Walk in") ||
-                            instructionLis.includes("Drive through");
-                        return (
-                            distanceInKm < radiusKm &&
-                            isOpenToday &&
-                            walkInOrDriveThru
-                        );
-                    }
-                );
-                matchedFilter.sort(
-                    (
-                        { lat: locationALat, lng: locationALng },
-                        { lat: locationBLat, lng: locationBLng }
-                    ) => {
-                        const distanceKmLocationA = getDistanceKm(
-                            lat,
-                            lng,
-                            locationALat,
-                            locationALng
-                        );
-                        const distanceKmLocationB = getDistanceKm(
-                            lat,
-                            lng,
-                            locationBLat,
-                            locationBLng
-                        );
-                        return distanceKmLocationA - distanceKmLocationB;
-                    }
-                );
-                setWalkinLocation(matchedFilter.slice(0, 6));
-            })
-            .finally(() => setLoading(false));
-    }, [lat, lng, radiusKm]);
-    const openModal = (locationIndex: number) => {
-        setSelectedLocation(locationIndex);
-    };
+            const walkInOrDriveThru =
+              instructionLis.includes("Walk in") ||
+              instructionLis.includes("Drive through");
+            return (
+              distanceInKm < radiusKm &&
+              isOpenToday &&
+              walkInOrDriveThru
+            );
+          }
+        );
+        matchedFilter.sort(
+          (
+            { lat: locationALat, lng: locationALng },
+            { lat: locationBLat, lng: locationBLng }
+          ) => {
+            const distanceKmLocationA = getDistanceKm(
+              lat,
+              lng,
+              locationALat,
+              locationALng
+            );
+            const distanceKmLocationB = getDistanceKm(
+              lat,
+              lng,
+              locationBLat,
+              locationBLng
+            );
+            return distanceKmLocationA - distanceKmLocationB;
+          }
+        );
+        setWalkinLocation(matchedFilter.slice(0, 6));
+      })
+      .finally(() => setLoading(false));
+  }, [lat, lng, radiusKm]);
+  const openModal = (locationIndex: number) => {
+    setSelectedLocation(locationIndex);
+  };
 
-    const clearSelectedLocation = () => setSelectedLocation(undefined);
+  const clearSelectedLocation = () => setSelectedLocation(undefined);
 
-    return (
-        <div>
-            <WalkModal
-                clearSelectedLocation={clearSelectedLocation}
-                location={
-                    selectedLocationIndex !== undefined
-                        ? walkInLocations[selectedLocationIndex]
-                        : undefined
-                }
-            />
-            <h2 className="WalkSection">
-                Walk-in &amp; Drive-through Locations{" "}
-            </h2>
-            {loading && (
-                <div
-                    style={{
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        marginTop: "1rem",
-                    }}
-                >
-                    <Spinner color="black" />
-                    <div
-                        style={{
-                            marginLeft: "1rem",
-                            fontSize: "1.5rem",
-                        }}
-                    >
-                        Loading...
-                    </div>
-                </div>
-            )}
-            {walkInLocations.length === 0 && !loading && (
-                <div>
-                    someone please come up with something that I can show when
-                    no walk in location found. Looking at you, WALTER LIM XOXOXO
-                    <br /> Minh pls ill design something soon im tired
-                </div>
-            )}
-            <WalkContainer>
-                {walkInLocations.map(
-                    (
-                        {
-                            name,
-                            isOpenToday,
-                            lat: locationLat,
-                            lng: locationLng,
-                        },
-                        index
-                    ) => {
-                        return (
-                            <WalkBox
-                                onClick={() => openModal(index)}
-                                key={index}
-                            >
-                                <section>
-                                    <div>
-                                        <h3>{name}</h3>
-                                        {locationLat && locationLng && (
-                                            <p>
-                                                {Math.round(
-                                                    getDistanceKm(
-                                                        lat,
-                                                        lng,
-                                                        locationLat,
-                                                        locationLng
-                                                    ) * 10
-                                                ) / 10}
-                                                KM away
-                                            </p>
-                                        )}
-                                    </div>
-
-                                    {isOpenToday && <p>Open today </p>}
-                                </section>
-                                <img
-                                    className="Chevron"
-                                    src="./arrow-right-1.svg"
-                                    alt=""
-                                />
-                            </WalkBox>
-                        );
-                    }
-                )}
-            </WalkContainer>
+  return (
+    <div>
+      <WalkModal
+        clearSelectedLocation={clearSelectedLocation}
+        location={
+          selectedLocationIndex !== undefined
+            ? walkInLocations[selectedLocationIndex]
+            : undefined
+        }
+      />
+      <h2 className="WalkSection">
+        Walk-in &amp; Drive-through Locations{" "}
+      </h2>
+      {loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "1rem",
+          }}
+        >
+          <Spinner color="black" />
+          <div
+            style={{
+              marginLeft: "1rem",
+              fontSize: "1.5rem",
+            }}
+          >
+            Loading...
+          </div>
         </div>
-    );
+      )}
+      {walkInLocations.length === 0 && !loading && (
+        <div>
+          someone please come up with something that I can show when
+          no walk in location found. Looking at you, WALTER LIM XOXOXO
+          <br /> Minh pls ill design something soon im tired
+        </div>
+      )}
+      <WalkContainer>
+        {walkInLocations.map(
+          (
+            {
+              name,
+              isOpenToday,
+              lat: locationLat,
+              lng: locationLng,
+            },
+            index
+          ) => {
+            return (
+              <WalkBox
+                onClick={() => openModal(index)}
+                key={index}
+              >
+                <section>
+                  <div>
+                    <h3>{name}</h3>
+                    {locationLat && locationLng && (
+                      <p>
+                        {Math.round(
+                          getDistanceKm(
+                            lat,
+                            lng,
+                            locationLat,
+                            locationLng
+                          ) * 10
+                        ) / 10}
+                        KM away
+                      </p>
+                    )}
+                  </div>
+
+                  {isOpenToday && <p>Open today </p>}
+                </section>
+                <img
+                  className="Chevron"
+                  src="./arrow-right-1.svg"
+                  alt=""
+                />
+              </WalkBox>
+            );
+          }
+        )}
+      </WalkContainer>
+    </div>
+  );
 }
