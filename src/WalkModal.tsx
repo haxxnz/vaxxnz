aeb1f6e7448e952648b3905896e3fa46aa7588ef
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-07 00:09:40 +1200

    telephone bs

--- src/WalkModal.tsx
+++ src/WalkModal.tsx
@@ -30,6 +30,7 @@ const WalkModal = ({ clearSelectedLocation, location }: Props) => {
   if (location == null) {
     return null;
   }
+  const telephone = location.telephone.replace(/\[.*\]/g, "");
 
   return (
     <Modal
@@ -131,7 +132,7 @@ const WalkModal = ({ clearSelectedLocation, location }: Props) => {
                 })}
             </p> */}
 
-          {location.telephone && (
+          {telephone && (
             <p
               style={{
                 marginBottom: "0.5rem",
@@ -144,7 +145,7 @@ const WalkModal = ({ clearSelectedLocation, location }: Props) => {
             >
               <h3> Phone</h3>
 
-              <a href={`tel:${location.telephone}`}>{location.telephone}</a>
+              <a href={`tel:${telephone}`}>{telephone}</a>
             </p>
           )}
 
