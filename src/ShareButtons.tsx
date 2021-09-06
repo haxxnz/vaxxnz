e3551e036a2f00e537f3db4566e16c0ed0af8d1a
Will Seagar <w.seagar@gmail.com>
2021-09-06 18:54:53 +1200

    Fix formatting

--- src/ShareButtons.tsx
+++ src/ShareButtons.tsx
@@ -1,5 +1,4 @@
-
-import React from 'react';
+import React from "react";
 import {
   FacebookShareButton,
   FacebookMessengerShareButton,
@@ -13,68 +12,53 @@ import {
   LinkedinIcon,
   RedditIcon,
   EmailIcon,
-} from 'react-share';
-
+} from "react-share";
 
 export const ShareButtons = () => {
-    const shareUrl = 'https://vaxx.nz';
-    const title = 'Vaxx.nz | NZ COVID Vaccination Finder';
+  const shareUrl = "https://vaxx.nz";
+  const title = "Vaxx.nz | NZ COVID Vaccination Finder";
 
-    return (
-      <>
-       <div>
-          <FacebookShareButton
-            url={shareUrl}
-            quote={title}
-          >
-            <FacebookIcon size={32} round />
-          </FacebookShareButton>
-        </div>
+  return (
+    <>
+      <div>
+        <FacebookShareButton url={shareUrl} quote={title}>
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+      </div>
 
-        <div>
-          <FacebookMessengerShareButton
-            url={shareUrl}
-            appId="1207927626340037"
-          >
-            <FacebookMessengerIcon size={32} round />
-          </FacebookMessengerShareButton>
-        </div>
+      <div>
+        <FacebookMessengerShareButton url={shareUrl} appId="1207927626340037">
+          <FacebookMessengerIcon size={32} round />
+        </FacebookMessengerShareButton>
+      </div>
 
-        <div>
-          <TwitterShareButton
-            url={shareUrl}
-            title={title}
-          >
-            <TwitterIcon size={32} round />
-          </TwitterShareButton>
-        </div>
+      <div>
+        <TwitterShareButton url={shareUrl} title={title}>
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+      </div>
 
-        <div>
-          <LinkedinShareButton url={shareUrl}>
-            <LinkedinIcon size={32} round />
-          </LinkedinShareButton>
-        </div>
+      <div>
+        <LinkedinShareButton url={shareUrl}>
+          <LinkedinIcon size={32} round />
+        </LinkedinShareButton>
+      </div>
 
-
-        <div>
-          <RedditShareButton
-            url={shareUrl}
-            title={title}
-            windowWidth={660}
-            windowHeight={460}
-          >
-            <RedditIcon size={32} round />
-          </RedditShareButton>
-        </div>
-        <div>
-          <EmailShareButton
-            url={shareUrl}
-            subject={title}
-            body="body"
-          >
-            <EmailIcon size={32} round />
-          </EmailShareButton>
-        </div>
-        </>
-    );
-}
+      <div>
+        <RedditShareButton
+          url={shareUrl}
+          title={title}
+          windowWidth={660}
+          windowHeight={460}
+        >
+          <RedditIcon size={32} round />
+        </RedditShareButton>
+      </div>
+      <div>
+        <EmailShareButton url={shareUrl} subject={title} body="body">
+          <EmailIcon size={32} round />
+        </EmailShareButton>
+      </div>
+    </>
+  );
+};
