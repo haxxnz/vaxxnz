3f77cb69f908e056b5ed8aff4a34aaf4e4c176a6
Oliver Cooper <oliver.cooper@me.com>
2021-09-06 20:11:31 +1200

    Prettier update

--- src/WalkModal.tsx
+++ src/WalkModal.tsx
@@ -6,46 +6,46 @@ import { WalkinLocation } from "./getData";
 import { ModalGrid } from "./VaxComponents";
 
 type Props = {
-    clearSelectedLocation: () => void;
-    location?: WalkinLocation;
+  clearSelectedLocation: () => void;
+  location?: WalkinLocation;
 };
 
 const WalkModal = ({ clearSelectedLocation, location }: Props) => {
-    const close = () => clearSelectedLocation();
-    if (location == null) {
-        return null;
-    }
+  const close = () => clearSelectedLocation();
+  if (location == null) {
+    return null;
+  }
 
-    return (
-        <Modal
-            onClose={close}
-            isOpen={!!location}
-            unstable_ModalBackdropScroll={true}
-            size="full"
-            overrides={{
-                Root: { style: { zIndex: 1500 } },
-                Dialog: {
-                    style: {
-                        height: "80vh",
-                        display: "flex",
-                        flexDirection: "column",
-                        padding: "1.5rem",
-                        maxWidth: "1200px",
-                    },
-                },
+  return (
+    <Modal
+      onClose={close}
+      isOpen={!!location}
+      unstable_ModalBackdropScroll={true}
+      size="full"
+      overrides={{
+        Root: { style: { zIndex: 1500 } },
+        Dialog: {
+          style: {
+            height: "80vh",
+            display: "flex",
+            flexDirection: "column",
+            padding: "1.5rem",
+            maxWidth: "1200px",
+          },
+        },
+      }}
+    >
+      <ModalGrid>
+        <div>
+          <h1
+            style={{
+              marginBottom: "1rem",
             }}
-        >
-            <ModalGrid>
-                <div>
-                    <h1
-                        style={{
-                            marginBottom: "1rem",
-                        }}
-                    >
-                        {location.name}
-                    </h1>
+          >
+            {location.name}
+          </h1>
 
-                    {/* <p
+          {/* <p
                 style={{
                     marginTop: "1rem",
                     paddingTop: "1.25rem",
@@ -67,145 +67,139 @@ const WalkModal = ({ clearSelectedLocation, location }: Props) => {
                     );
                 })}
             </p> */}
-                    {location.opennningHours.schedule && (
-                        <p
-                            style={{
-                                marginTop: "1rem",
-                                marginBottom: "0.5rem",
-                                marginRight: "1rem",
-                                fontSize: "1.25rem",
-                                borderBottom: "1px solid lightgray",
-                                borderTop: "1px solid lightgray",
-                                paddingBottom: "1.5rem",
-                                paddingTop: "1rem",
-                                lineHeight: "1.5",
-                            }}
-                        >
-                            <strong> Hours</strong>
-                            <br />
-                            {Object.keys(location.opennningHours.schedule).map(
-                                (openDate, index) => {
-                                    return (
-                                        <Fragment key={index}>
-                                            {openDate}{" "}
-                                            {
-                                                location.opennningHours
-                                                    .schedule[openDate]
-                                            }
-                                            <br />
-                                        </Fragment>
-                                    );
-                                }
-                            )}
-                        </p>
-                    )}
+          {location.opennningHours.schedule && (
+            <p
+              style={{
+                marginTop: "1rem",
+                marginBottom: "0.5rem",
+                marginRight: "1rem",
+                fontSize: "1.25rem",
+                borderBottom: "1px solid lightgray",
+                borderTop: "1px solid lightgray",
+                paddingBottom: "1.5rem",
+                paddingTop: "1rem",
+                lineHeight: "1.5",
+              }}
+            >
+              <strong> Hours</strong>
+              <br />
+              {Object.keys(location.opennningHours.schedule).map(
+                (openDate, index) => {
+                  return (
+                    <Fragment key={index}>
+                      {openDate} {location.opennningHours.schedule[openDate]}
+                      <br />
+                    </Fragment>
+                  );
+                }
+              )}
+            </p>
+          )}
 
-                    {Object.entries(location.opennningHours.exceptions).map(
-                        ([key, value], index) => {
-                            return (
-                                <div
-                                    style={{
-                                        marginTop: "1rem",
-                                        lineHeight: "1.5",
-                                    }}
-                                >
-                                    <strong>{key}</strong>
-                                    <br />
+          {Object.entries(location.opennningHours.exceptions).map(
+            ([key, value], index) => {
+              return (
+                <div
+                  style={{
+                    marginTop: "1rem",
+                    lineHeight: "1.5",
+                  }}
+                >
+                  <strong>{key}</strong>
+                  <br />
 
-                                    <p key={index}>{value}</p>
-                                </div>
-                            );
-                        }
-                    )}
-                    {location.opennningHours.notes.map((note, index) => {
-                        return (
-                            <div style={{ marginTop: "1rem" }}>
-                                <small key={index}>{note}</small>
-                            </div>
-                        );
-                    })}
-
-                    {location.telephone && (
-                        <p
-                            style={{
-                                marginTop: "1.5rem",
-                                marginBottom: "0.5rem",
-                                marginRight: "1rem",
-                                fontSize: "1.25rem",
-
-                                paddingTop: "1rem",
-                                borderTop: "1px solid lightgray",
-                                lineHeight: "1.5",
-                            }}
-                        >
-                            <strong> Phone</strong>
-                            <br />
-
-                            <a href={`tel:${location.telephone}`}>
-                                {location.telephone}
-                            </a>
-                        </p>
-                    )}
-
-                    <p
-                        style={{
-                            marginTop: "1rem",
-                            marginBottom: "0.5rem",
-                            fontSize: "1.25rem",
-                            paddingTop: "1.5rem",
-                            paddingBottom: "1rem",
-                            borderTop: "1px solid lightgray",
-                            lineHeight: "1.5",
-                        }}
-                    >
-                        <strong> Address</strong>
-                        <br />
-                        {location.address}
-                    </p>
-                    <Button
-                        overrides={{
-                            Root: {
-                                style: {
-                                    width: "100%",
-                                    marginTop: "0.5rem",
-                                    marginRight: 0,
-                                    marginBottom: "0.5rem",
-                                    marginLeft: 0,
-                                },
-                            },
-                        }}
-                        kind={KIND.primary}
-                        onClick={() => {
-                            // Also close the modal to avoid confusing stuff
-                            window.open(
-                                `https://www.google.com/maps/dir/?api=1&saddr=My+Location&destination=${location.address}`
-                            );
-                            close();
-                        }}
-                    >
-                        Get directions
-                    </Button>
-                    <Button
-                        overrides={{
-                            Root: {
-                                style: {
-                                    width: "100%",
-                                    marginTop: "0.5rem",
-                                    marginRight: 0,
-                                    marginBottom: "0.5rem",
-                                    marginLeft: 0,
-                                },
-                            },
-                        }}
-                        kind={KIND.secondary}
-                        onClick={close}
-                    >
-                        Cancel
-                    </Button>
+                  <p key={index}>{value}</p>
                 </div>
-            </ModalGrid>
-        </Modal>
-    );
+              );
+            }
+          )}
+          {location.opennningHours.notes.map((note, index) => {
+            return (
+              <div style={{ marginTop: "1rem" }}>
+                <small key={index}>{note}</small>
+              </div>
+            );
+          })}
+
+          {location.telephone && (
+            <p
+              style={{
+                marginTop: "1.5rem",
+                marginBottom: "0.5rem",
+                marginRight: "1rem",
+                fontSize: "1.25rem",
+
+                paddingTop: "1rem",
+                borderTop: "1px solid lightgray",
+                lineHeight: "1.5",
+              }}
+            >
+              <strong> Phone</strong>
+              <br />
+
+              <a href={`tel:${location.telephone}`}>{location.telephone}</a>
+            </p>
+          )}
+
+          <p
+            style={{
+              marginTop: "1rem",
+              marginBottom: "0.5rem",
+              fontSize: "1.25rem",
+              paddingTop: "1.5rem",
+              paddingBottom: "1rem",
+              borderTop: "1px solid lightgray",
+              lineHeight: "1.5",
+            }}
+          >
+            <strong> Address</strong>
+            <br />
+            {location.address}
+          </p>
+          <Button
+            overrides={{
+              Root: {
+                style: {
+                  width: "100%",
+                  marginTop: "0.5rem",
+                  marginRight: 0,
+                  marginBottom: "0.5rem",
+                  marginLeft: 0,
+                },
+              },
+            }}
+            kind={KIND.primary}
+            onClick={() => {
+              // Also close the modal to avoid confusing stuff
+              window.open(
+                `https://www.google.com/maps/dir/?api=1&saddr=My+Location&destination=${location.address}`
+              );
+              close();
+            }}
+          >
+            Get directions
+          </Button>
+          <Button
+            overrides={{
+              Root: {
+                style: {
+                  width: "100%",
+                  marginTop: "0.5rem",
+                  marginRight: 0,
+                  marginBottom: "0.5rem",
+                  marginLeft: 0,
+                },
+              },
+            }}
+            kind={KIND.secondary}
+            onClick={close}
+          >
+            Cancel
+          </Button>
+        </div>
+      </ModalGrid>
+    </Modal>
+  );
 };
 
 export default WalkModal;
