23aec828451c63e7861d9bb6e3e720470a03af6a
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-09-07 15:06:39 +1000

    formatting

--- src/RadiusSelect.tsx
+++ src/RadiusSelect.tsx
@@ -1,44 +1,44 @@
 import { Select } from "baseui/select";
 
 const options = [
-    { label: "Within 2km", id: 2 },
-    { label: "Within 5km", id: 5 },
-    { label: "Within 10km", id: 10 },
-    { label: "Within 25km", id: 25 },
-    { label: "Within 50km", id: 50 },
-    { label: "Within 100km", id: 100 },
+  { label: "Within 2km", id: 2 },
+  { label: "Within 5km", id: 5 },
+  { label: "Within 10km", id: 10 },
+  { label: "Within 25km", id: 25 },
+  { label: "Within 50km", id: 50 },
+  { label: "Within 100km", id: 100 },
 ];
 
 interface Props {
-    value: number;
-    setValue: (value: number) => void;
+  value: number;
+  setValue: (value: number) => void;
 }
 
 export default function RadiusSelect(props: Props) {
-    const selectedOption = options.find((o) => o.id === props.value);
-    return (
-        <Select
-            overrides={{
-                Root: {
-                    style: {
-                        maxHeight: "40px",
-                    },
-                },
-            }}
-            clearable={false}
-            options={options}
-            value={selectedOption ? [selectedOption] : []}
-            placeholder="Select radius"
-            onChange={(params) => {
-                const selectedOptions = params.value;
-                if (selectedOptions.length > 0) {
-                    const selectedOption = selectedOptions[0];
-                    const id = selectedOption.id;
-                    if (id && typeof id === "number") {
-                        props.setValue(id);
-                    }
-                }
-            }}
-        />
-    );
+  const selectedOption = options.find((o) => o.id === props.value);
+  return (
+    <Select
+      overrides={{
+        Root: {
+          style: {
+            maxHeight: "40px",
+          },
+        },
+      }}
+      clearable={false}
+      options={options}
+      value={selectedOption ? [selectedOption] : []}
+      placeholder="Select radius"
+      onChange={(params) => {
+        const selectedOptions = params.value;
+        if (selectedOptions.length > 0) {
+          const selectedOption = selectedOptions[0];
+          const id = selectedOption.id;
+          if (id && typeof id === "number") {
+            props.setValue(id);
+          }
+        }
+      }}
+    />
+  );
 }
