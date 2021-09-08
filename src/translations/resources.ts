6ec0de77266150cec32a267ef4bc74fc41331284
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-09-08 22:31:04 +1000

    Added Spanish

--- src/translations/resources.ts
+++ src/translations/resources.ts
@@ -1,5 +1,5 @@
 import common_en from "./en/common.json";
-// import common_es from "./es/common.json";
+import common_es from "./es/common.json";
 import enFlag from "./flags/gbFlag.png";
 // import miFlag from "./flags/miFlag.png";
 // import esFlag from "./flags/miFlag.png";
@@ -18,15 +18,18 @@ const languages = [
   //   code: "mi",
   //   flag: miFlag,
   // },
-  // {
-  //   common: common_es,
-  //   label: "Spanish",
-  //   code: "es",
-  //   flag: esFlag
-  // }
+  {
+    common: common_es,
+    label: "Spanish",
+    code: "es",
+    // flag: esFlag
+  },
 ];
 
-const resources = languages.reduce((current, lang) => ({ ...current, [lang["code"]]: lang }), {});
+const resources = languages.reduce(
+  (current, lang) => ({ ...current, [lang["code"]]: lang }),
+  {}
+);
 
 export { resources };
 
