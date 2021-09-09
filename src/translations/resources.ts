f350404211a313c4847da9cc4ae00bc66ac1e912
Francis Malloch-Boe <frazboyz101@gmail.com>
2021-09-09 15:03:35 +1200

    Fixed language selector

--- src/translations/resources.ts
+++ src/translations/resources.ts
@@ -6,30 +6,42 @@ import enFlag from "./flags/gbFlag.png";
 // import miFlag from "./flags/miFlag.png";
 // import esFlag from "./flags/miFlag.png";
 
+export type Language = {
+  common: typeof common_en,
+  label: string;
+  code: string;
+  flag?: string;
+  id: string;
+}
+
 const languages = [
   {
     common: common_en,
     label: "English",
     code: "en",
     flag: enFlag,
+    id: 'en',
   },
   {
     common: common_es,
     label: "Español",
     code: "es",
     // flag: esFlag
+    id: 'es'
   },
   {
     common: common_de,
     label: "Deutsch",
     code: "de",
     // flag: deFlag,
+    id: "de",
   },
   {
     common: common_ru,
     label: "Русский",
     code: "ru",
     //  flag: enFlag
+    id: "ru"
   }
 ];
 
