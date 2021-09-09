c4de73b32e06b8b2923d29b0bf2bcfd7fdaf5a9e
Jimmy Page <12769660+jimmymasaru@users.noreply.github.com>
2021-09-09 18:57:57 +1200

    Add translation for Simplified Chinese and Traditional Chinese.

--- src/translations/resources.ts
+++ src/translations/resources.ts
@@ -2,6 +2,8 @@ import common_en from "./locales/common_en.json";
 import common_de from "./locales/common_de.json";
 import common_ru from "./locales/common_ru.json";
 import common_es from "./locales/common_es.json";
+import common_zh_cn from "./locales/common_zh_cn.json";
+import common_zh_tw from "./locales/common_zh_tw.json";
 
 const languages = [
   {
@@ -23,6 +25,16 @@ const languages = [
     common: common_ru,
     label: "Русский",
     code: "ru",
+  },
+  {
+    common: common_zh_cn,
+    label: "中文（简体）",
+    code: "zh-CN",
+  },
+  {
+    common: common_zh_tw,
+    label: "中文（繁體）",
+    code: "zh-TW",
   },
 ];
 
