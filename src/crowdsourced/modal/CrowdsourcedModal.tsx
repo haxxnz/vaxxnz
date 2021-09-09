39d25b269c17b76711d97b43707d8ddae113275d
Francis Malloch-Boe <frazboyz101@gmail.com>
2021-09-10 08:29:59 +1200

    Reverting src changes

--- src/crowdsourced/modal/CrowdsourcedModal.tsx
+++ src/crowdsourced/modal/CrowdsourcedModal.tsx
@@ -115,9 +115,7 @@ const CrowdsourcedModal: FunctionComponent<Props> = ({
           </Button>
 
           <NoticeList>
-            <NoticeListItem
-              title={t("walkins.otherLocations.disclaimer.title")}
-            >
+            <NoticeListItem title={t("walkins.otherLocations.disclaimer.title")}>
               {t("walkins.otherLocations.disclaimer.message")}
             </NoticeListItem>
           </NoticeList>
