115732b365dbc89ccafb9098c91d8390db17c16a
JacksonLaw <jlawrence.inbox@gmail.com>
2021-09-10 20:06:20 +1200

    Add closed state

--- src/crowdsourced/modal/CrowdsourcedModal.tsx
+++ src/crowdsourced/modal/CrowdsourcedModal.tsx
@@ -176,8 +176,9 @@ const CrowdsourcedModal: FunctionComponent<Props> = ({
               {location.openingHours.map((oh, index) => {
                 return (
                   <p key={index}>
-                    {dayOfWeekToString(oh.day)}{" "}
-                    {oh.isOpen ? oh.hours : undefined}
+                    {dayOfWeekToString(oh.day)}
+                    {": "}
+                    {oh.isOpen ? oh.hours : t("walkins.closed")}
                     <br />
                   </p>
                 );
