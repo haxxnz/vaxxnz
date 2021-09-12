c1e0126b34af35bd4e7f24ffa7893b89229912a6
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2021-09-12 19:06:15 +1000

    add polish

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,4 +1,4 @@
-import { enNZ, de, es, ru, vi, zhCN, zhTW, mn } from "date-fns/locale"; // Only import locale we support. Don't use import * it is bad
+import { enNZ, de, es, ru, vi, zhCN, zhTW, mn, pl } from "date-fns/locale"; // Only import locale we support. Don't use import * it is bad
 import { Locale } from "date-fns";
 import i18next from "i18next";
 
@@ -8,6 +8,7 @@ const supportedDateLocale: { [localeString: string]: Locale } = {
   "ms-MY": mn, // Same issue
   "es-ES": es,
   "ru-RU": ru,
+  "pl-PL": pl,
   "vi-VN": vi, // Don't ask me i18 locale string is different than date-fns locale string so ¯\_(ツ)_/¯
   "zh-TW": zhTW,
   "zh-CN": zhCN,
