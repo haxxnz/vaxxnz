667a756cf42b8b3ff9309aa35e2e6b1c82386fae
Ramon Quitales <me@rquitales.com>
2021-09-14 21:35:59 +1200

    refactor: set default value for comparator param
    Make comparator an optional value inn sortByAsc for more reusable code
    in cases where we do not need to transform array elements for
    comparisons.
    
    Signed-off-by: Ramon Quitales <me@rquitales.com>

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,7 +1,20 @@
+/**
+ * Returns a new array with elements sorted in ascending order.
+ * A custom transformation function can be supplied to be used to
+ * generate the value to be compared - useful for example, when we
+ * have an array of co-ordinates that we want to sort by the absolute
+ * difference in distance from a given point.
+ *
+ * @export
+ * @template T
+ * @param {T[]} notes
+ * @param {(note: T) => T} [comparator=(x) => x]
+ * @return {T[]} Sorted array.
+ */
 export function sortByAsc<T = unknown>(
   notes: T[],
-  comparator: (note: T) => string | number
-) {
+  comparator: (note: T) => T = (x) => x
+): T[] {
   return [...notes].sort((a: T, b: T) => {
     if (comparator(a) < comparator(b)) {
       return -1;
