a9121e19b7d0afcde4ff304a28f7039061be4fda
Ramon Quitales <me@rquitales.com>
2021-09-14 12:23:10 +0000

    chore: add docstrings for constants
    Signed-off-by: Ramon Quitales <me@rquitales.com>

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,11 +1,40 @@
+/**
+ * API key to access addressfinder.nz services.
+ *
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ADDRESS_FINDER_API_KEY = "ARFHPVK67QXM49BEWDL3";
 
+/**
+ * addressfinder.nz API endpoint for reverse geocoding.
+ *
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const REVERSE_GEOCODE_URL =
   "https://api.addressfinder.io/api/nz/address/reverse_geocode/?";
 
+/**
+ * addressfinder.nz API endpoint for obtaining detailed information
+ * for a given address/location.
+ *
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const ADDRESS_METADATA_URL =
   "https://api.addressfinder.io/api/nz/address/metadata/?";
 
+/**
+ * The default/starting location to populate vaxx.nz.
+ *
+ * @constant
+ * @type {string}
+ * @default
+ */
 export const DEFAULT_LOCATION = {
   lat: -36.853610199274385,
   lng: 174.76054541484535,
