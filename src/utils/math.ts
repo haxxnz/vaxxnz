8a6844b106aea38152f25576679b00f6e0e3acad
Ramon Quitales <me@rquitales.com>
2021-09-14 21:34:13 +1200

    refactor: add better documentation for sum func
    Signed-off-by: Ramon Quitales <me@rquitales.com>

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,3 +1,10 @@
-export function sum(array: number[]) {
+/**
+ * Returns the sum of an array of numbers.
+ *
+ * @export
+ * @param {number[]} array
+ * @return {number}
+ */
+export function sum(array: number[]): number {
   return array.reduce((a, b) => a + b, 0);
 }
