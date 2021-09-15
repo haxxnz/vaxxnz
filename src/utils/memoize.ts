d7049517a2a6d4f5c9dc1cab71772a7656efe239
Ramon Quitales <me@rquitales.com>
2021-09-15 11:14:42 +0000

    refactor: add documentation for memoizeOnce
    Signed-off-by: Ramon Quitales <me@rquitales.com>

--- src/utils/memoize.ts
+++ src/utils/memoize.ts
@@ -1,11 +1,20 @@
-export function memoizeOnce<T = unknown>(fn: () => T) {
+/**
+ * Caches the output of a function call so subsequent calls don't
+ * need to be re-computed.
+ *
+ * @export
+ * @template T
+ * @param {() => T} fn
+ * @return {*}
+ */
+export function memoizeOnce<T = unknown>(fn: () => T): any {
   let cache: T;
   return function () {
     if (cache) {
       return cache;
     }
-    const result = fn();
-    cache = result;
-    return result;
+
+    cache = fn();
+    return cache;
   };
 }
