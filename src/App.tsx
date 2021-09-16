ecc47eb31e26588a3e15a4b1116f899a2174c580
Abhijnan-Bajpai <abhijnanbajpai@outlook.com>
2021-09-16 16:53:21 +0530

    Added a boundary box condition on the presence of the cookie bar

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,10 @@ import { enqueueAnalyticsEvent } from "./utils/analytics";
 import { DEFAULT_LOCATION } from "./utils/consts";
 import { useSearchParams } from "./utils/url";
 import { TodayLocationsSection } from "./today-locations/TodayLocationsSection";
-import CookieConsent from "react-cookie-consent";
+import CookiesBar from "./Cookies";
 
 function App() {
   const { lat, lng } = useSearchParams();
-  const EUbbox = [-12.0792755076,35.9674116355,40.6576894187,58.9861052075]
   const [coords, setCoords] = useState({
     lat: DEFAULT_LOCATION.lat,
     lng: DEFAULT_LOCATION.lng,
@@ -177,34 +176,7 @@ function App() {
             backgroundImage: `url(${process.env.PUBLIC_URL + "./bg.svg"})`,
           }}
         ></div>
-        <CookieConsent
-          location="bottom"
-          buttonText="I Understand"
-          declineButtonText="Decline Cookies"
-          cookieName="myAwesomeCookieName2"
-          style={{ background: "#000000" }}
-          buttonStyle={{
-            color: "#4e503b",
-            fontSize: "13px",
-            fontWeight: "Bold",
-          }}
-          declineButtonStyle={{
-            background: "#000000",
-            textDecoration: "underline",
-          }}
-          expires={150}
-          onAccept={(byScroll) => {
-            
-          }}
-          enableDeclineButton
-          onDecline={() => {
-            (window as { [key: string]: any })['ga-disable-G-PXHVR76F27']=true
-          }}
-        >
-          <p style={{ fontFamily: "sans-serif" }}>
-            This website uses cookies to enhance the user experience.
-          </p>
-        </CookieConsent>
+        <CookiesBar lng={coords.lng} lat={coords.lat}></CookiesBar>
       </div>
     </>
   );
