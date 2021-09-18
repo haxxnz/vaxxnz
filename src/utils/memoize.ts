cc5b2c12cd2425f3a2091f91bcf3ad56faad15de
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-18 20:44:58 +1200

    new function just dropped: memoize0

--- src/utils/memoize.ts
+++ src/utils/memoize.ts
@@ -18,3 +18,26 @@ export function memoizeOnce<T = unknown>(fn: () => T) {
     return cache;
   };
 }
+
+/**
+ * Caches the output of a function call so subsequent calls don't
+ * need to be re-computed. Based on first argument. Requires first argument.
+ *
+ * @export
+ * @template T
+ * @template A
+ * @param {(a: A) => T} fn
+ * @return {*}
+ */
+export function memoize0<T = unknown, A = unknown>(fn: (a: A) => T) {
+  const cache: Record<string, T> = {};
+  return (a: A) => {
+    const key = a + "";
+    if (cache[key]) {
+      return cache[key];
+    }
+
+    cache[key] = fn(a);
+    return cache[key];
+  };
+}
