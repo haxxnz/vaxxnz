0ea5723d09f08ae73dd4b2225add6620841a1cbc
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-18 21:52:09 +1200

    split off Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useMemo, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
 import "./App.css";
 import { CalendarSection } from "./calendar/CalendarSection";
-import LanguageSelect from "./LanguageSelect";
 import { LocationPicker } from "./location-picker/LocationPicker";
 import { DEFAULT_LOCATION } from "./utils/consts";
 import { useSearchParams } from "./utils/url";
@@ -13,6 +12,7 @@ import { Switch, Route, useLocation } from "react-router-dom";
 import { useBookingData } from "./calendar/booking/BookingData";
 import { LocationRouter } from "./LocationRouter";
 import { Footer } from "./Footer";
+import { Header } from "./Header";
 
 function App() {
   const { lat, lng, radius } = useSearchParams();
@@ -39,30 +39,7 @@ function App() {
   return (
     <>
       <div className="App">
-        <header className="menu-header">
-          <a href="/" className="nolink menu-logo">
-            {t("core.title")}
-          </a>
-          <div className="menu-divider">
-            {" "}
-            <a
-              href="https://github.com/CovidEngine/vaxxnz"
-              target="_blank"
-              rel="noreferrer"
-              className="menu-link"
-            >
-              {t("navigation.about")}
-            </a>
-            <a
-              href="https://airtable.com/shrxuw3vSp2yRPrG7"
-              className="menu-link"
-            >
-              {t("navigation.contact")}
-            </a>
-            <LanguageSelect />
-          </div>
-        </header>
-
+        <Header />
         <div className={"big-old-container"}>
           <Switch>
             <Route path="/bookings/:date">
