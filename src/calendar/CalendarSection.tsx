184472f7400026cb69df7c5266ed532591e320fa
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-22 19:18:32 +1200

    add error state for when out of new zealand

--- src/calendar/CalendarSection.tsx
+++ src/calendar/CalendarSection.tsx
@@ -2,15 +2,32 @@ import React, { FunctionComponent } from "react";
 import { BookingCalendar, LoadingBookingCalendar } from "./Calendar";
 import { BookingDataResult } from "./booking/BookingData";
 import { CalendarError } from "./CalendarError";
+import { useCoords } from "../utils/useCoords";
 
 export interface CalendarSectionProps {
   bookingData: BookingDataResult;
 }
+const NZbbox = [166.509144322, -46.641235447, 178.517093541, -34.4506617165];
 
 /** Loads booking data, display the calendar or load errors. */
 export const CalendarSection: FunctionComponent<CalendarSectionProps> = ({
   bookingData: data,
 }) => {
+  const coords = useCoords();
+  if (
+    !(
+      coords.lat > NZbbox[1] &&
+      coords.lat < NZbbox[3] &&
+      coords.lng > NZbbox[0] &&
+      coords.lng < NZbbox[2]
+    )
+  ) {
+    return (
+      <CalendarError
+        errorMessage={"Uh oh... this page is only visible in New Zealand"}
+      />
+    );
+  }
   return (
     <>
       {"ok" in data ? (
