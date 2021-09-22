fbfc7b4c234e0b0bda62697a1befca78f8b49774
JacksonLaw <jlawrence.inbox@gmail.com>
2021-09-22 23:00:02 +1200

    fix warnings

--- src/utils/parsePhone.ts
+++ src/utils/parsePhone.ts
@@ -1,11 +1,11 @@
 //Takes in a string and returns either the first valid phone number or original string
 
 export function parsePhoneNumber(potentialNumber: string | undefined): string {
-  let phoneRegex = /[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.0-9]*/;
+  let phoneRegex = /[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s.0-9]*/;
   let eightHundredRegex = /0800\s[A-Z]+/;
   //Does a standard phone regex, but that will also just return the 0800 from a number like 0800 ORANGA
   //So the function check its length and do an 0800 regex as well
-  if (potentialNumber == undefined || null) {
+  if (potentialNumber === undefined || null) {
     return "";
   } else if (
     potentialNumber.match(phoneRegex) != null &&
