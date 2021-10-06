4e70668da63b4816c86bbcad51c16f55a0455af6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-10-06 18:08:49 +1300

    add footer to FAQ

--- src/faq.tsx
+++ src/faq.tsx
@@ -1,43 +1,48 @@
+import { Footer } from "./Footer";
+
 const faq = () => {
   return (
-    <div style={{ margin: "3rem" }}>
-      <h2>Frequently Asked Questions</h2>
-      <p
-        style={{
-          marginTop: "1rem",
-          marginBottom: "0.5rem",
-          marginRight: "1rem",
-        }}
-      ></p>
-      <br />
-      <p>
-        <h4>How do I get my vaccination location added to Vaxx.nz?</h4>
-        <p>
-          We automatically load any locations listed on bookmyvaccine.nz, but if
-          you're not integrated with them, contact Healthpoint to profile your
-          missing location:{" "}
-          <a href="https://www.healthpoint.co.nz/register/">
-            https://www.healthpoint.co.nz/register/
-          </a>
-        </p>
+    <>
+      <div style={{ margin: "3rem" }}>
+        <h2>Frequently Asked Questions</h2>
+        <p
+          style={{
+            marginTop: "1rem",
+            marginBottom: "0.5rem",
+            marginRight: "1rem",
+          }}
+        ></p>
         <br />
-        <h4>Who maintains Vaxx.nz?</h4>
         <p>
-          Vaxx.nz is an open source community project run by volunteers:{" "}
-          <a href="https://github.com/CovidEngine/vaxxnz#built-by">
-            https://github.com/CovidEngine/vaxxnz#built-by
-          </a>
+          <h4>How do I get my vaccination location added to Vaxx.nz?</h4>
+          <p>
+            We automatically load any locations listed on bookmyvaccine.nz, but
+            if you're not integrated with them, contact Healthpoint to profile
+            your missing location:{" "}
+            <a href="https://www.healthpoint.co.nz/register/">
+              https://www.healthpoint.co.nz/register/
+            </a>
+          </p>
+          <br />
+          <h4>Who maintains Vaxx.nz?</h4>
+          <p>
+            Vaxx.nz is an open source community project run by volunteers:{" "}
+            <a href="https://github.com/CovidEngine/vaxxnz#built-by">
+              https://github.com/CovidEngine/vaxxnz#built-by
+            </a>
+          </p>
+          <br />
+          <h4>How do I get involved?</h4>
+          <p>
+            Head over to{" "}
+            <a href="https://github.com/CovidEngine/vaxxnz/blob/main/CONTRIBUTING.md">
+              https://github.com/CovidEngine/vaxxnz/blob/main/CONTRIBUTING.md
+            </a>
+          </p>
         </p>
-        <br />
-        <h4>How do I get involved?</h4>
-        <p>
-          Head over to{" "}
-          <a href="https://github.com/CovidEngine/vaxxnz/blob/main/CONTRIBUTING.md">
-            https://github.com/CovidEngine/vaxxnz/blob/main/CONTRIBUTING.md
-          </a>
-        </p>
-      </p>
-    </div>
+      </div>
+      <Footer />
+    </>
   );
 };
 
